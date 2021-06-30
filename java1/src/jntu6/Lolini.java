164c0e264ad9ab64622c5e424a01e4b1ac4ea4a7
qaplanet <qaplanet@qaplanet-PC>
2021-06-30 13:15:56 +0530

    test

---
+++ java1/src/jntu6/Lolini.java
@@ -0,0 +1,7 @@
+package jntu6;
+
+public interface Lolini {
+public static final int sno = 0;
+public static final String stdname = "";
+
+}
---
+++ java1/src/p1/Jntu22.java
@@ -0,0 +1,16 @@
+package p1;
+
+public class Jntu22  extends Jntu19{
+	void deelet(){
+		int a;
+		int b;
+		int t;
+		a=40;
+		b=20;
+		t=a-b;
+		System.out.println(t);
+				
+		
+	}
+
+}
